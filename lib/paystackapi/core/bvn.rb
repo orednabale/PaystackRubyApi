afecf37744e2c3d16d49d17a676659d7fb0c9ab1
Elmer Labandero <elmerzlabandero@gmail.com>
2020-10-25 14:29:27 +0000

    Add the Bearer word in Authorization requrest

--- lib/paystackapi/core/bvn.rb
+++ lib/paystackapi/core/bvn.rb
@@ -1,7 +1,9 @@
 module Verification
 	def self.resolve_bvn(body)
+		puts "My secret key is: "
+		puts ENV['PAYSTACK_SECRET_KEY']
 		api = HTTParty.get("#{API::BASE_URL}" + "#{API::RESOLVE_BVN_PATH}/" + "#{body}",
-			:headers => { "Authorization"=> ENV["PAYSTACK_SECRET_KEY"], "content-type" => "application/json"})
+			:headers => { "Authorization" => "Bearer "+ENV["PAYSTACK_SECRET_KEY"], "content-type" => "application/json"})
 		return api
 	end
 end
